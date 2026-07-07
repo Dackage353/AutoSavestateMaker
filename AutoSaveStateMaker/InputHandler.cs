619528090cff076f97a7e4c40bfa1f72553813ae
Daniel <daniel.g.hackel@gmail.com>
2026-07-06 20:48:48 -0500

    input handler cleanup

--- AutoSaveStateMaker/InputHandler.cs
+++ AutoSaveStateMaker/InputHandler.cs
@@ -7,36 +7,35 @@ namespace AutoSaveStateMaker
 {
     internal class InputHandler
     {
-        [DllImport("user32.dll")]
-        private static extern int SetForegroundWindow(IntPtr point);
-
         public Joystick joystick;
-        public int ACount, BCount, ZCount, StartCount,
-            LCount, RCount, CUpCount, CDownCount, CLeftCount,
-            CRightCount, DPadUpCount, DPadDownCount, DPadLeftCount, DPadRightCount;
 
-        public Action AAction = () => { },
-            BAction = () => { },
-            ZAction = () => { },
-            StartAction = () => { },
-            dPadUpAction = () => { },
-            dPadDownAction = () => { },
-            dPadLeftAction = () => { },
-            dPadRightAction = () => { };
-
+        public Action AAction { get; set; } = () => { };
+        public Action BAction { get; set; } = () => { };
+        public Action ZAction { get; set; } = () => { };
+        public Action StartAction { get; set; } = () => { };
+        public Action LAction { get; set; } = () => { };
+        public Action RAction { get; set; } = () => { };
+        public Action CUpAction { get; set; } = () => { };
+        public Action CDownAction { get; set; } = () => { };
+        public Action CLeftAction { get; set; } = () => { };
+        public Action CRightAction { get; set; } = () => { };
+        public Action DPadUpAction { get; set; } = () => { };
+        public Action DPadDownAction { get; set; } = () => { };
+        public Action DPadLeftAction { get; set; } = () => { };
+        public Action DPadRightAction { get; set; } = () => { };
 
         private ActiveButtons oldButtons, activeButtons = new();
 
         public bool HotkeysOn { get; set; } = false;
         public bool RequireR { get; set; } = false;
 
-        private readonly System.Windows.Forms.Timer timer = new();
+        private readonly System.Windows.Forms.Timer _timer = new();
 
         public void SetUpJoystick()
         {
-            timer.Interval = 6;
-            timer.Tick += ProcessJoystick;
-            timer.Start();
+            _timer.Interval = 6;
+            _timer.Tick += ProcessJoystick;
+            _timer.Start();
 
             try
             {
@@ -71,7 +70,7 @@ namespace AutoSaveStateMaker
                 }
 
             }
-            catch (SharpDXException e)
+            catch (SharpDXException)
             {
                 Debug.WriteLine("sharpdx exception");
             }
@@ -87,37 +86,26 @@ namespace AutoSaveStateMaker
                 var datas = joystick.GetBufferedData();
                 oldButtons = activeButtons;
 
-                foreach (var state in datas)
                 {
-                    bool pressed = state.Value != 0;
-
-                    switch (state.Offset)
-                    {
-                        case JoystickOffset.Buttons0: activeButtons.A = pressed; ACount++; break;
-                        case JoystickOffset.Buttons1: activeButtons.B = pressed; BCount++; break;
-                        case JoystickOffset.Buttons2: activeButtons.Z = pressed; ZCount++; break;
-                        case JoystickOffset.Buttons3: activeButtons.Start = pressed; StartCount++; break;
-                        case JoystickOffset.Buttons4: activeButtons.L = pressed; LCount++; break;
-                        case JoystickOffset.Buttons5: activeButtons.R = pressed; RCount++; break;
-                        case JoystickOffset.Buttons6: activeButtons.CUp = pressed; CUpCount++; break;
-                        case JoystickOffset.Buttons7: activeButtons.CDown = pressed; CDownCount++; break;
-                        case JoystickOffset.Buttons8: activeButtons.CLeft = pressed; CLeftCount++; break;
-                        case JoystickOffset.Buttons9: activeButtons.CRight = pressed; CRightCount++; break;
-                        case JoystickOffset.Buttons10: activeButtons.DPadUp = pressed; DPadUpCount++; break;
-                        case JoystickOffset.Buttons11: activeButtons.DPadDown = pressed; DPadDownCount++; break;
-                        case JoystickOffset.Buttons12: activeButtons.DPadLeft = pressed; DPadLeftCount++; break;
-                        case JoystickOffset.Buttons13: activeButtons.DPadRight = pressed; DPadRightCount++; break;
-                    }
                 }
 
                 bool rTest = !RequireR || activeButtons.R;
                 if (HotkeysOn && rTest)
                 {
-                    if (activeButtons.DPadUp && !oldButtons.DPadUp) dPadUpAction();
-                    if (activeButtons.DPadDown && !oldButtons.DPadDown) dPadDownAction();
-                    if (activeButtons.DPadLeft && !oldButtons.DPadLeft) dPadLeftAction();
-                    if (activeButtons.DPadRight && !oldButtons.DPadRight) dPadRightAction();
                     if (activeButtons.A && !oldButtons.A) AAction();
+                    if (activeButtons.B && !oldButtons.B) BAction();
+                    if (activeButtons.Z && !oldButtons.Z) ZAction();
+                    if (activeButtons.Start && !oldButtons.Start) StartAction();
+                    if (activeButtons.L && !oldButtons.L) LAction();
+                    if (activeButtons.R && !oldButtons.R) RAction();
+                    if (activeButtons.CUp && !oldButtons.CUp) CUpAction();
+                    if (activeButtons.CDown && !oldButtons.CDown) CDownAction();
+                    if (activeButtons.CLeft && !oldButtons.CLeft) CLeftAction();
+                    if (activeButtons.CRight && !oldButtons.CRight) CRightAction();
+                    if (activeButtons.DPadUp && !oldButtons.DPadUp) DPadUpAction();
+                    if (activeButtons.DPadDown && !oldButtons.DPadDown) DPadDownAction();
+                    if (activeButtons.DPadLeft && !oldButtons.DPadLeft) DPadLeftAction();
+                    if (activeButtons.DPadRight && !oldButtons.DPadRight) DPadRightAction();
                 }
             }
             catch (SharpDXException)
