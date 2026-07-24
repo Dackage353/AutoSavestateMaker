b68777a8ae0ba29d9ec38c07eb0fee1004afef5a
Daniel <daniel.g.hackel@gmail.com>
2026-07-23 21:28:52 -0500

    add input edit dialog

---
+++ AutoSavestateMaker/Input/InputEditDialog.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace AutoSavestateMaker.Input
+{
+    partial class InputEditDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            focusGameHotkeySet_Button = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            startStopHotkeySet_Button = new Button();
+            loadSavestateHotkeySet_Button = new Button();
+            slotLeftHotkeySet_Button = new Button();
+            slotRightHotkeySet_Button = new Button();
+            focusGameHotkeyValue_Label = new Label();
+            startStopHotkeyValue_Label = new Label();
+            loadSavestateHotkeyValue_Label = new Label();
+            slotLeftHotkeyValue_Label = new Label();
+            slotRightHotkeyValue_Label = new Label();
+            ok_Button = new Button();
+            label1 = new Label();
+            shiftHotkeySet_Button = new Button();
+            shiftHotkeyValue_Label = new Label();
+            SuspendLayout();
+            // 
+            // focusGameHotkeySet_Button
+            // 
+            focusGameHotkeySet_Button.Location = new Point(185, 12);
+            focusGameHotkeySet_Button.Name = "focusGameHotkeySet_Button";
+            focusGameHotkeySet_Button.Size = new Size(75, 23);
+            focusGameHotkeySet_Button.TabIndex = 3;
+            focusGameHotkeySet_Button.Text = "assign...";
+            focusGameHotkeySet_Button.UseVisualStyleBackColor = true;
+            focusGameHotkeySet_Button.Click += focusGameHotkeySet_Button_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(91, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Focus Game (A)";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(15, 45);
+            label3.Name = "label3";
+            label3.Size = new Size(114, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Start Stop (Dpad up)";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(15, 75);
+            label4.Name = "label4";
+            label4.Size = new Size(157, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Load Savestate (Dpad down)";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(15, 105);
+            label5.Name = "label5";
+            label5.Size = new Size(109, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Slot Left (Dpad left)";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(15, 135);
+            label6.Name = "label6";
+            label6.Size = new Size(125, 15);
+            label6.TabIndex = 4;
+            label6.Text = "Slot Right (Dpad right)";
+            // 
+            // startStopHotkeySet_Button
+            // 
+            startStopHotkeySet_Button.Location = new Point(185, 42);
+            startStopHotkeySet_Button.Name = "startStopHotkeySet_Button";
+            startStopHotkeySet_Button.Size = new Size(75, 23);
+            startStopHotkeySet_Button.TabIndex = 3;
+            startStopHotkeySet_Button.Text = "assign...";
+            startStopHotkeySet_Button.UseVisualStyleBackColor = true;
+            startStopHotkeySet_Button.Click += startStopHotkeySet_Button_Click;
+            // 
+            // loadSavestateHotkeySet_Button
+            // 
+            loadSavestateHotkeySet_Button.Location = new Point(185, 72);
+            loadSavestateHotkeySet_Button.Name = "loadSavestateHotkeySet_Button";
+            loadSavestateHotkeySet_Button.Size = new Size(75, 23);
+            loadSavestateHotkeySet_Button.TabIndex = 3;
+            loadSavestateHotkeySet_Button.Text = "assign...";
+            loadSavestateHotkeySet_Button.UseVisualStyleBackColor = true;
+            loadSavestateHotkeySet_Button.Click += loadSavestateHotkeySet_Button_Click;
+            // 
+            // slotLeftHotkeySet_Button
+            // 
+            slotLeftHotkeySet_Button.Location = new Point(185, 102);
+            slotLeftHotkeySet_Button.Name = "slotLeftHotkeySet_Button";
+            slotLeftHotkeySet_Button.Size = new Size(75, 23);
+            slotLeftHotkeySet_Button.TabIndex = 3;
+            slotLeftHotkeySet_Button.Text = "assign...";
+            slotLeftHotkeySet_Button.UseVisualStyleBackColor = true;
+            slotLeftHotkeySet_Button.Click += slotLeftHotkeySet_Button_Click;
+            // 
+            // slotRightHotkeySet_Button
+            // 
+            slotRightHotkeySet_Button.Location = new Point(185, 132);
+            slotRightHotkeySet_Button.Name = "slotRightHotkeySet_Button";
+            slotRightHotkeySet_Button.Size = new Size(75, 23);
+            slotRightHotkeySet_Button.TabIndex = 3;
+            slotRightHotkeySet_Button.Text = "assign...";
+            slotRightHotkeySet_Button.UseVisualStyleBackColor = true;
+            slotRightHotkeySet_Button.Click += slotRightHotkeySet_Button_Click;
+            // 
+            // focusGameHotkeyValue_Label
+            // 
+            focusGameHotkeyValue_Label.AutoSize = true;
+            focusGameHotkeyValue_Label.Location = new Point(290, 15);
+            focusGameHotkeyValue_Label.Name = "focusGameHotkeyValue_Label";
+            focusGameHotkeyValue_Label.Size = new Size(38, 15);
+            focusGameHotkeyValue_Label.TabIndex = 5;
+            focusGameHotkeyValue_Label.Text = "label7";
+            // 
+            // startStopHotkeyValue_Label
+            // 
+            startStopHotkeyValue_Label.AutoSize = true;
+            startStopHotkeyValue_Label.Location = new Point(290, 45);
+            startStopHotkeyValue_Label.Name = "startStopHotkeyValue_Label";
+            startStopHotkeyValue_Label.Size = new Size(38, 15);
+            startStopHotkeyValue_Label.TabIndex = 6;
+            startStopHotkeyValue_Label.Text = "label8";
+            // 
+            // loadSavestateHotkeyValue_Label
+            // 
+            loadSavestateHotkeyValue_Label.AutoSize = true;
+            loadSavestateHotkeyValue_Label.Location = new Point(290, 75);
+            loadSavestateHotkeyValue_Label.Name = "loadSavestateHotkeyValue_Label";
+            loadSavestateHotkeyValue_Label.Size = new Size(38, 15);
+            loadSavestateHotkeyValue_Label.TabIndex = 6;
+            loadSavestateHotkeyValue_Label.Text = "label8";
+            // 
+            // slotLeftHotkeyValue_Label
+            // 
+            slotLeftHotkeyValue_Label.AutoSize = true;
+            slotLeftHotkeyValue_Label.Location = new Point(290, 105);
+            slotLeftHotkeyValue_Label.Name = "slotLeftHotkeyValue_Label";
+            slotLeftHotkeyValue_Label.Size = new Size(38, 15);
+            slotLeftHotkeyValue_Label.TabIndex = 6;
+            slotLeftHotkeyValue_Label.Text = "label8";
+            // 
+            // slotRightHotkeyValue_Label
+            // 
+            slotRightHotkeyValue_Label.AutoSize = true;
+            slotRightHotkeyValue_Label.Location = new Point(290, 135);
+            slotRightHotkeyValue_Label.Name = "slotRightHotkeyValue_Label";
+            slotRightHotkeyValue_Label.Size = new Size(38, 15);
+            slotRightHotkeyValue_Label.TabIndex = 6;
+            slotRightHotkeyValue_Label.Text = "label8";
+            // 
+            // ok_Button
+            // 
+            ok_Button.Location = new Point(321, 209);
+            ok_Button.Name = "ok_Button";
+            ok_Button.Size = new Size(75, 23);
+            ok_Button.TabIndex = 7;
+            ok_Button.Text = "OK";
+            ok_Button.UseVisualStyleBackColor = true;
+            ok_Button.Click += ok_Button_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 165);
+            label1.Name = "label1";
+            label1.Size = new Size(106, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Shift (Optional, R?)";
+            // 
+            // shiftHotkeySet_Button
+            // 
+            shiftHotkeySet_Button.Location = new Point(185, 162);
+            shiftHotkeySet_Button.Name = "shiftHotkeySet_Button";
+            shiftHotkeySet_Button.Size = new Size(75, 23);
+            shiftHotkeySet_Button.TabIndex = 8;
+            shiftHotkeySet_Button.Text = "assign...";
+            shiftHotkeySet_Button.UseVisualStyleBackColor = true;
+            shiftHotkeySet_Button.Click += shiftHotkeySet_Button_Click;
+            // 
+            // shiftHotkeyValue_Label
+            // 
+            shiftHotkeyValue_Label.AutoSize = true;
+            shiftHotkeyValue_Label.Location = new Point(290, 165);
+            shiftHotkeyValue_Label.Name = "shiftHotkeyValue_Label";
+            shiftHotkeyValue_Label.Size = new Size(38, 15);
+            shiftHotkeyValue_Label.TabIndex = 6;
+            shiftHotkeyValue_Label.Text = "label8";
+            // 
+            // InputEditDialog
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(408, 244);
+            Controls.Add(shiftHotkeySet_Button);
+            Controls.Add(ok_Button);
+            Controls.Add(shiftHotkeyValue_Label);
+            Controls.Add(slotRightHotkeyValue_Label);
+            Controls.Add(slotLeftHotkeyValue_Label);
+            Controls.Add(loadSavestateHotkeyValue_Label);
+            Controls.Add(startStopHotkeyValue_Label);
+            Controls.Add(focusGameHotkeyValue_Label);
+            Controls.Add(label1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(slotRightHotkeySet_Button);
+            Controls.Add(slotLeftHotkeySet_Button);
+            Controls.Add(loadSavestateHotkeySet_Button);
+            Controls.Add(startStopHotkeySet_Button);
+            Controls.Add(focusGameHotkeySet_Button);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "InputEditDialog";
+            Text = "Button Test";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Button focusGameHotkeySet_Button;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button startStopHotkeySet_Button;
+        private Button loadSavestateHotkeySet_Button;
+        private Button slotLeftHotkeySet_Button;
+        private Button slotRightHotkeySet_Button;
+        private Label focusGameHotkeyValue_Label;
+        private Label startStopHotkeyValue_Label;
+        private Label loadSavestateHotkeyValue_Label;
+        private Label slotLeftHotkeyValue_Label;
+        private Label slotRightHotkeyValue_Label;
+        private Button ok_Button;
+        private Label label1;
+        private Button shiftHotkeySet_Button;
+        private Label shiftHotkeyValue_Label;
+    }
+}
---
+++ AutoSavestateMaker/Input/InputEditDialog.cs
@@ -0,0 +1,166 @@
+﻿using SDL;
+
+namespace AutoSavestateMaker.Input
+{
+    internal partial class InputEditDialog : Form
+    {
+        private InputHandler _inputHandler;
+
+        public InputEditDialog(InputHandler inputHandler)
+        {
+            InitializeComponent();
+
+            _inputHandler = inputHandler;
+            _inputHandler.InEditMode = true;
+
+            focusGameHotkeyValue_Label.Text = Config.Instance.FocusGame.ToString();
+            startStopHotkeyValue_Label.Text = Config.Instance.StartStop.ToString();
+            loadSavestateHotkeyValue_Label.Text = Config.Instance.LoadSavestate.ToString();
+            slotLeftHotkeyValue_Label.Text = Config.Instance.SlotLeft.ToString();
+            slotRightHotkeyValue_Label.Text = Config.Instance.SlotRight.ToString();
+            shiftHotkeyValue_Label.Text = Config.Instance.Shift.ToString();
+
+            FormClosed += (a, b) =>
+            {
+                _inputHandler.InEditMode = false;
+                Config.SaveInstance();
+            };
+        }
+
+        private void focusGameHotkeySet_Button_Click(object sender, EventArgs e)
+        {
+            focusGameHotkeyValue_Label.Text = "waiting...";
+
+            _inputHandler.AssignWaitingAction = (e) =>
+            {
+                if (IsValidEvent(e))
+                {
+                    Config.Instance.FocusGame = GetInputInfo(e);
+                    focusGameHotkeyValue_Label.Text = Config.Instance.FocusGame.ToString();
+                    return true;
+                }
+
+                return false;
+            };
+        }
+
+        private void startStopHotkeySet_Button_Click(object sender, EventArgs e)
+        {
+            startStopHotkeyValue_Label.Text = "waiting...";
+
+            _inputHandler.AssignWaitingAction = (e) =>
+            {
+                if (IsValidEvent(e))
+                {
+                    Config.Instance.StartStop = GetInputInfo(e);
+                    startStopHotkeyValue_Label.Text = Config.Instance.StartStop.ToString();
+                    return true;
+                }
+
+                return false;
+            };
+        }
+
+        private void loadSavestateHotkeySet_Button_Click(object sender, EventArgs e)
+        {
+            loadSavestateHotkeyValue_Label.Text = "waiting...";
+
+            _inputHandler.AssignWaitingAction = (e) =>
+            {
+                if (IsValidEvent(e))
+                {
+                    Config.Instance.LoadSavestate = GetInputInfo(e);
+                    loadSavestateHotkeyValue_Label.Text = Config.Instance.LoadSavestate.ToString();
+                    return true;
+                }
+
+                return false;
+            };
+        }
+
+        private void slotLeftHotkeySet_Button_Click(object sender, EventArgs e)
+        {
+            slotLeftHotkeyValue_Label.Text = "waiting...";
+
+            _inputHandler.AssignWaitingAction = (e) =>
+            {
+                if (IsValidEvent(e))
+                {
+                    Config.Instance.SlotLeft = GetInputInfo(e);
+                    slotLeftHotkeyValue_Label.Text = Config.Instance.SlotLeft.ToString();
+                    return true;
+                }
+
+                return false;
+            };
+        }
+
+        private void slotRightHotkeySet_Button_Click(object sender, EventArgs e)
+        {
+            slotRightHotkeyValue_Label.Text = "waiting...";
+
+            _inputHandler.AssignWaitingAction = (e) =>
+            {
+                if (IsValidEvent(e))
+                {
+                    Config.Instance.SlotRight = GetInputInfo(e);
+                    slotRightHotkeyValue_Label.Text = Config.Instance.SlotRight.ToString();
+                    return true;
+                }
+
+                return false;
+            };
+        }
+
+        private void shiftHotkeySet_Button_Click(object sender, EventArgs e)
+        {
+            shiftHotkeyValue_Label.Text = "waiting...";
+
+            _inputHandler.AssignWaitingAction = (e) =>
+            {
+                if (IsValidEvent(e))
+                {
+                    Config.Instance.Shift = GetInputInfo(e);
+                    shiftHotkeyValue_Label.Text = Config.Instance.Shift.ToString();
+                    return true;
+                }
+
+                return false;
+            };
+        }
+
+        private InputInfo GetInputInfo(SDL_Event e)
+        {
+            switch ((SDL_EventType)e.type)
+            {
+                case SDL_EventType.SDL_EVENT_JOYSTICK_BUTTON_DOWN:
+                    return new InputInfo(InputType.Button, e.jbutton.button);
+                case SDL_EventType.SDL_EVENT_JOYSTICK_HAT_MOTION:
+                    return new InputInfo(InputType.DPad, e.jhat.hat) { DPadValue = e.jhat.value };
+                case SDL_EventType.SDL_EVENT_JOYSTICK_AXIS_MOTION:
+                    return new InputInfo(InputType.Axis, e.jaxis.axis) { AxisDirection = e.jaxis.value > 0 ? 1 : -1 };
+            }
+
+            return null;
+        }
+
+        private bool IsValidEvent(SDL_Event e)
+        {
+            SDL_EventType type = (SDL_EventType)e.type;
+
+            if (type == SDL_EventType.SDL_EVENT_JOYSTICK_HAT_MOTION)
+            {
+                return e.jhat.value == SDL3.SDL_HAT_LEFT || e.jhat.value == SDL3.SDL_HAT_RIGHT || e.jhat.value == SDL3.SDL_HAT_UP || e.jhat.value == SDL3.SDL_HAT_DOWN;
+            }
+
+            return type == SDL_EventType.SDL_EVENT_JOYSTICK_BUTTON_DOWN
+                || type == SDL_EventType.SDL_EVENT_JOYSTICK_AXIS_MOTION
+                || type == SDL_EventType.SDL_EVENT_JOYSTICK_HAT_MOTION;
+        }
+
+        private void ok_Button_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
